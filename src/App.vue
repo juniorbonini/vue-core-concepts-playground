6a0366584a639cad70c6e27dbb4406c58aca04a2
juniorbonini <juniorboninia7x@gmail.com>
2026-02-25 15:18:39 -0300

    feat: Novo component Welcome criado.

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,46 @@ import HelloWorld from "./components/HelloWorld/HelloWorld.vue";
 </script>
 
 <template>
-  <HelloWorld message="Você conseguiu!" />
+  <header>
+    <img
+      class="logoImage"
+      src="./images/logo.svg"
+      alt="logo vite"
+      width="125"
+      height="125"
+    />
+    <div class="container">
+      <HelloWorld message="Você conseguiu!" />
+    </div>
+  </header>
+
 </template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logoImage {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 64rem) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  .container {
+    display: flex;
+    place-content: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
