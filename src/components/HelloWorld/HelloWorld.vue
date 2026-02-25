e56e1011c545bbf46bb5fb29259a0b679b078002
juniorbonini <juniorboninia7x@gmail.com>
2026-02-25 15:01:55 -0300

    feat: HelloWorld component criado.

---
+++ src/components/HelloWorld/HelloWorld.vue
@@ -0,0 +1,44 @@
+<script setup>
+defineProps({
+  message: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="title">{{ message }}</h1>
+    <h3>
+      Você criou com sucesso um projeto com
+      <a href="https://vite.dev/">Vite</a> +
+      <a href="https://vuejs.org/">Vue3</a>
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: realtive;
+  top: -0.625rem;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.container h1,
+.container h3 {
+  text-align: center;
+}
+
+@media (min-width: 64rem) {
+  .container h1,
+  .container h3 {
+    text-align: left;
+  }
+}
+</style>
